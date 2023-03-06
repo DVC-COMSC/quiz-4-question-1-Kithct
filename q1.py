2ab4e3770004011e4c11225f1c8cd0d791ee8e48
Zelin Chen <6692czl@gmail.com>
2023-03-06 14:09:34 -0800

    1

--- q1.py
+++ q1.py
@@ -2,7 +2,18 @@
 # ******************************
 # Make your Code
 # ******************************
-
+cn = 0
+pn = 0
+i = 0
+c = 0
+while i < 10:
+    n = int(input())
+    cn = n
+    if cn % 2 == 0 and pn % 2 == 0 and cn != 0 and pn != 0:
+        c += 1
+    pn = cn
+    i += 1
+print(c)
 
 # Requirement
 # No need to use list
